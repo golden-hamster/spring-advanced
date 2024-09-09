5fcf00b6ad38952fea6657aa1f00258a8df82fc3
golden-hamster <isack19999@gmail.com>
2024-09-09 23:25:03 +0900

    레벨 1-4: JWT 유효성 검사 로직 수정

--- src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
+++ src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.manager.controller;
 
 import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.config.JwtUtil;
@@ -38,12 +39,11 @@ public class ManagerController {
 
     @DeleteMapping("/todos/{todoId}/managers/{managerId}")
     public void deleteManager(
-            @RequestHeader("Authorization") String bearerToken,
+            HttpServletRequest request,
             @PathVariable long todoId,
             @PathVariable long managerId
     ) {
-        Claims claims = jwtUtil.extractClaims(bearerToken.substring(7));
-        long userId = Long.parseLong(claims.getSubject());
+        Long userId = (Long) request.getAttribute("userId");
         managerService.deleteManager(userId, todoId, managerId);
     }
 }
